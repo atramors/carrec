7f8bff5ddd3f0bdabc6c48c25ab09556f74fe237
atramors <blackworkarts@gmail.com>
2020-08-11 18:07:31 +0300

    Create first model

--- data_rec.py
+++ data_rec.py
@@ -1,24 +1,107 @@
+#%%
+import matplotlib.pyplot as plt
+import os
 import numpy as np
 import tensorflow as tf
+from PIL import Image
 from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
 
 # Path to images
 DATA_DIR = "/Users/atramors/desktop/My_Projects/datasets/car_DS/car_data/\
 car_data/"
 
+# Create a dataset
 # Parameters
 batch_size = 64
 img_height = 200
 img_width = 200
 
 train_dataset = keras.preprocessing.image_dataset_from_directory(
-    DATA_DIR + "train", labels="inferred",
+    DATA_DIR + "train",
+    labels="inferred",
     color_mode="rgb",
     batch_size=batch_size,
-    image_size=(img_height, img_height))
+    image_size=(img_height, img_height),
+)
 test_dataset = keras.preprocessing.image_dataset_from_directory(
-    DATA_DIR + "test", labels="inferred",
+    DATA_DIR + "test",
+    labels="inferred",
     color_mode="rgb",
     batch_size=batch_size,
-    image_size=(img_height, img_height))
+    image_size=(img_height, img_height),
+)
+class_names = train_dataset.class_names
 
+
+# Visualize the data
+plt.figure(figsize=(10, 10))
+for images, labels in train_dataset.take(1):
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        plt.title(class_names[labels[i]])
+        plt.axis("off")
+
+# Configure the dataset for performance
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+train_dataset = train_dataset.cache().shuffle(8000).prefetch(buffer_size=AUTOTUNE)
+test_dataset = test_dataset.cache().shuffle(8000).prefetch(buffer_size=AUTOTUNE)
+
+
+# Create the model
+num_classes = 196
+
+model = Sequential(
+    [
+        layers.experimental.preprocessing.Rescaling(
+            1.0 / 255, input_shape=(img_height, img_width, 3)
+        ),
+        layers.Conv2D(16, 3, padding="same", activation="relu"),
+        layers.MaxPooling2D(),
+        layers.Conv2D(32, 3, padding="same", activation="relu"),
+        layers.MaxPooling2D(),
+        layers.Conv2D(64, 3, padding="same", activation="relu"),
+        layers.MaxPooling2D(),
+        layers.Flatten(),
+        layers.Dense(128, activation="relu"),
+        layers.Dense(num_classes),
+    ]
+)
+
+# Compile the model
+
+model.compile(
+    optimizer="adam",
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=["accuracy"],
+)
+
+model.summary()
+
+epochs = 10
+history = model.fit(train_dataset, validation_data=test_dataset, epochs=epochs)
+
+# Visualize training results
+acc = history.history["accuracy"]
+val_acc = history.history["val_accuracy"]
+
+loss = history.history["loss"]
+val_loss = history.history["val_loss"]
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label="Training Accuracy")
+plt.plot(epochs_range, val_acc, label="Validation Accuracy")
+plt.legend(loc="lower right")
+plt.title("Training and Validation Accuracy")
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label="Training Loss")
+plt.plot(epochs_range, val_loss, label="Validation Loss")
+plt.legend(loc="upper right")
+plt.title("Training and Validation Loss")
+plt.show()
