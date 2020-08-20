64467ea4db341e949e532a4bdb2ab6479ab34133
atramors <blackworkarts@gmail.com>
2020-08-20 23:35:01 +0300

    Cleaned new model

--- data_rec.py
+++ data_rec.py
@@ -1,25 +1,30 @@
 #%%
+import mlflow
+import mlflow.tensorflow
 import matplotlib.pyplot as plt
 import os
 import numpy as np
 import tensorflow as tf
-from PIL import Image
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 
+
+mlflow.tensorflow.autolog()
+
 # Path to images
 DATA_DIR = "/Users/atramors/desktop/My_Projects/datasets/car_DS/car_data/\
-car_data/"
+car_data/train"
 
 # Create a dataset
 # Parameters
 batch_size = 200
 img_height = 200
 img_width = 200
+num_classes = 196
 
 train_dataset = keras.preprocessing.image_dataset_from_directory(
-    DATA_DIR + "train",
+    DATA_DIR,
     labels="inferred",
     validation_split=0.2,
     subset="training",
@@ -29,7 +34,7 @@ train_dataset = keras.preprocessing.image_dataset_from_directory(
     image_size=(img_height, img_height),
 )
 valid_dataset = keras.preprocessing.image_dataset_from_directory(
-    DATA_DIR + "train",
+    DATA_DIR,
     labels="inferred",
     validation_split=0.2,
     subset="validation",
@@ -39,40 +44,33 @@ valid_dataset = keras.preprocessing.image_dataset_from_directory(
     image_size=(img_height, img_height),
 )
 
-# test_dataset = keras.preprocessing.image_dataset_from_directory(
-#     DATA_DIR + "test",
-#     labels="inferred",
-#     color_mode="rgb",
-#     batch_size=batch_size,
-#     image_size=(img_height, img_height),
-# )
 class_names = train_dataset.class_names
-
 
 # Configure the dataset for performance
 AUTOTUNE = tf.data.experimental.AUTOTUNE
-train_dataset = train_dataset.cache().shuffle(8144).prefetch(buffer_size=AUTOTUNE)
+train_dataset = train_dataset.cache().shuffle(2700).prefetch(buffer_size=AUTOTUNE)
 valid_dataset = valid_dataset.cache().prefetch(buffer_size=AUTOTUNE)
 
-
-data_augmentation = keras.Sequential(
-    [
-        layers.experimental.preprocessing.RandomFlip(
-            "horizontal", input_shape=(img_height, img_width, 3)
-        ),
-        layers.experimental.preprocessing.RandomRotation(0.1),
-        layers.experimental.preprocessing.RandomZoom(0.1),
-    ]
-)
+# data_augmentation = keras.Sequential(
+#     [
+#         layers.experimental.preprocessing.RandomFlip(
+#             "horizontal", input_shape=(img_height, img_width, 3)
+#         ),
+#         layers.experimental.preprocessing.RandomRotation(0.1),
+#         layers.experimental.preprocessing.RandomZoom(0.1),
+#     ]
+# )
 
 # Create the model
-num_classes = 196
 
 model = Sequential(
     [
-        data_augmentation,
+        layers.experimental.preprocessing.RandomFlip(
+            "horizontal_and_vertical", input_shape=(img_height, img_width, 3)
+        ),
+        layers.experimental.preprocessing.RandomRotation(0.1),
+        layers.experimental.preprocessing.RandomZoom(0.1),
         layers.experimental.preprocessing.Rescaling(1.0 / 255),
-        # layers.experimental.preprocessing.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
         layers.Conv2D(16, 3, padding="same", activation="relu"),
         layers.MaxPooling2D(),
         layers.Conv2D(32, 3, padding="same", activation="relu"),
@@ -82,7 +80,7 @@ model = Sequential(
         layers.Dropout(0.2),
         layers.Flatten(),
         layers.Dense(128, activation="relu"),
-        layers.Dense(num_classes),
+        layers.Dense(num_classes, activation="relu"),
     ]
 )
 
@@ -91,23 +89,20 @@ model = Sequential(
 model.compile(
     optimizer="adam",
     loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    # loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
     metrics=["accuracy"],
 )
 
 model.summary()
 
-epochs = 5
+epochs = 10
 history = model.fit(train_dataset, validation_data=valid_dataset, epochs=epochs)
 
 # Visualize training results
 
 acc = history.history["accuracy"]
 val_acc = history.history["val_accuracy"]
-
 loss = history.history["loss"]
 val_loss = history.history["val_loss"]
-
 epochs_range = range(epochs)
 
 plt.figure(figsize=(8, 8))
