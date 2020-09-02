bc73de056d2e7294aae23cfb26678044b7120a25
Roman D <blackworkarts@gmail.com>
2020-09-03 01:19:59 +0300

    Created test file

---
+++ test.py
@@ -0,0 +1,16 @@
+import unittest
+from unittest import mock
+
+
+
+class TestRequestMethods(unittest.TestCase):
+    def test_post(self,):
+
+        return
+
+    def test_get_user(self, uid):
+        return 
+
+
+if __name__ == "__main__":
+    unittest.main()
