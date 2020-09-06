9f6492f3c6a3b2ad157fa8815fb8099040236715
Ilya Pekelny <ilya@launchpadrecruits.com>
2020-09-06 10:10:21 +0300

    Remove redundant lines.

--- test.py
+++ test.py
@@ -5,12 +5,8 @@ from unittest import mock
 
 class TestRequestMethods(unittest.TestCase):
     def test_post(self,):
-
         return
 
     def test_get_user(self, uid):
         return 
 
-
-if __name__ == "__main__":
-    unittest.main()
