e455014e1decd86b17596f35f2924eda50dc7615
Roman D <blackworkarts@gmail.com>
2020-09-10 20:56:43 +0300

    Cleanup

--- test.py
+++ test.py
@@ -8,16 +8,13 @@ from carapp import app, db, db_models, endpoints
 class TestRequestMethods(unittest.TestCase):
     def test_get_user(self, MockUser):
         with app.test_client() as client:
-
             test_user = MockUser()
             test_user.query.return_value = {
                 "id": 112,
                 "email": "a1one@nosatana.com",
                 "account_type": "client",
             }
-
             response = client.get("/user/112")
-
             self.assertIsNotNone(response)
             self.assertIsInstance(response.get_json(), dict)
             self.assertEqual(response.status_code, 200)
@@ -25,16 +22,12 @@ class TestRequestMethods(unittest.TestCase):
 
     def test_get_user_failure(self, MockUser):
         with app.test_client() as client:
-
             test_user = MockUser()
             test_user.query.return_value = {"Reason": "User not found", "user_id": 2}
-
             response = client.get("/user/2")
-
             self.assertIsNotNone(response)
             self.assertIsInstance(response.get_json(), dict)
             self.assertEqual(response.status_code, 404)
 
     # def test_get_all_users(self, user_id):
     #     return
-
