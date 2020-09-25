f41ef38c296929a9b3716af629e7c9b87658cdf3
Roman D <blackworkarts@gmail.com>
2020-09-25 18:27:19 +0300

    Delete test added

--- test.py
+++ test.py
@@ -3,6 +3,12 @@ import json
 from unittest import mock
 from carapp import app, db, db_models, endpoints
 
+test_user = {
+    "id": 100000,
+    "username": "Ken1",
+    "account_type": "client",
+}
+
 
 @mock.patch.object(endpoints, "User")
 # Easier solution (check endpoints.py)
@@ -10,32 +16,64 @@ from carapp import app, db, db_models, endpoints
 class TestRequestMethods(unittest.TestCase):
     def test_get_user(self, MockUser):
         with app.test_client() as client:
-            MockUser.query.get.return_value = {
-                "id": 2,
-                "email": "ane@nosat.com",
-                "account_type": "client",
-            }
-            response = client.get("/user/2")
+            MockUser.query.get.return_value = test_user
+            response = client.get("/user/100000")
             self.assertIsNotNone(response)
             self.assertIsInstance(response.get_json(), dict)
             self.assertEqual(response.status_code, 200)
-            self.assertEqual(response.get_json()["id"], 2)
+            self.assertEqual(response.get_json()["id"], 100000)
 
     def test_get_user_failure(self, MockUser):
         with app.test_client() as client:
             MockUser.query.get.return_value = None
-            response = client.get("/user/2")
+            response = client.get("/user/100000")
             self.assertIsNotNone(response)
             self.assertIsInstance(response.get_json(), dict)
             self.assertEqual(response.status_code, 404)
 
-    # def test_get_all_users(self, MockUser):
-    #     with app.test_client() as client:
-    #         test_users = MockUser()
-    #         test_users.query.all.return_value = [
-    #             {"id": 112, "email": "a1one@nosatana.com", "account_type": "client",},
-    #         ]
-    #         response = client.get("/users")
-    #         self.assertIsNotNone(response)
-    #         self.assertIsInstance(response.get_json()[0], dict)
-    #         self.assertEqual(response.status_code, 200)
+    def test_get_all_users(self, MockUser):
+        with app.test_client() as client:
+            MockUser.query.all.return_value = [
+                test_user,
+            ]
+            response = client.get("/users")
+            self.assertIsNotNone(response)
+            self.assertIsInstance(response.get_json()[0], dict)
+            self.assertEqual(response.status_code, 200)
+
+    def test_get_all_users_failure(self, MockUser):
+        with app.test_client() as client:
+            MockUser.query.all.return_value = None
+            response = client.get("/users")
+            self.assertIsNotNone(response)
+            self.assertIsInstance(response.get_json(), dict)
+            self.assertEqual(response.status_code, 500)
+
+    def test_post_user(self, MockUser):
+        with app.test_client() as client:
+            MockUser.return_value = test_user
+            response = client.post("/user", json=self.test_user)
+            import pdb; pdb.set_trace()
+            self.assertIsNotNone(response)
+            self.assertIsInstance(response.get_json(), dict)
+            self.assertEqual(response.status_code, 201)
+
+    def test_delete_user(self, MockUser):
+        with app.test_client() as client:
+            MockUser.query.get.return_value = test_user
+            response = client.delete("/user/100000")
+            self.assertIsNotNone(response)
+            self.assertIsInstance(response.get_json(), dict)
+            # self.assertEqual(response.status_code, 204)
+            # self.assertEqual(response.get_json(), {})
+
+    def test_delete_user_failure(self, MockUser):
+        with app.test_client() as client:
+            MockUser.query.get.return_value = None
+            response = client.delete("/user/100000")
+            self.assertIsNotNone(response)
+            self.assertIsInstance(response.get_json(), dict)
+            self.assertEqual(response.status_code, 404)
+            self.assertEqual(
+                response.get_json(), {"Reason": "No User with such id here!"}
+            )
