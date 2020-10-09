0da5a6b8c652f42b8725b5b94f2c1535ec67ebc3
Roman D <blackworkarts@gmail.com>
2020-10-09 22:34:37 +0300

    Start with Car-endpoints

--- carapp/endpoints.py
+++ carapp/endpoints.py
@@ -1,39 +1,89 @@
-import requests
-import carapp
-import json
 import logging
+import webargs.fields
 from carapp import api, app, bcrypt, db
 
 # from carapp import db_models
 from carapp.db_models import User, Car
-from flask import request, jsonify
 from flask_restful import Resource
-from marshmallow import Schema, fields, validate, ValidationError
-from webargs import fields
-from webargs.flaskparser import use_args, use_kwargs
-
+from marshmallow import Schema, fields, validate
+from webargs.flaskparser import use_kwargs
 
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(message)s")
 logger = logging.getLogger(__file__)
 
 user_args = {
-    "id": fields.Int(),
-    "username": fields.Str(required=True),
-    "password": fields.Str(validate=lambda p: len(p) >= 5),
-    "email": fields.Str(required=True),
-    "account_type": fields.Str(required=True),
+    "id": webargs.fields.Int(),
+    "username": webargs.fields.Str(required=True),
+    "password": webargs.fields.Str(required=True),
+    "email": webargs.fields.Str(required=True),
+    "account_type": webargs.fields.Str(required=True),
+}
+
+car_args = {
+    "id": webargs.fields.Int(),
+    "car_title": webargs.fields.Str(required=True),
+    "title": webargs.fields.Str(required=True),
+    "condition": webargs.fields.Str(required=True),
+    "body_type": webargs.fields.Str(required=True),
+    "brand": webargs.fields.Str(required=True),
+    "model": webargs.fields.Str(required=True),
+    "year_of_production": webargs.fields.Str(required=True),
+    "country_origin": webargs.fields.Str(required=True),
+    "country_now": webargs.fields.Str(required=True),
+    "city": webargs.fields.Str(required=True),
+    "mileage": webargs.fields.Str(required=True),
+    "technical_condition": webargs.fields.Str(required=True),
+    "gear_box": webargs.fields.Str(required=True),
+    "fuel_type": webargs.fields.Str(required=True),
+    "engine": webargs.fields.Str(required=True),
+    "color": webargs.fields.Str(required=True),
+    "safety": webargs.fields.Str(required=True),
+    "wanted": webargs.fields.Str(required=True),
+    "multimedia": webargs.fields.Str(required=True),
+    "comfort": webargs.fields.Str(required=True),
+    "picture": webargs.fields.Str(required=True),
+    "date_added": webargs.fields.DateTime(format="%(asctime)"),
+    "user_id": webargs.fields.Int(),
 }
 
 
 class UserSchema(Schema):
     id = fields.Int()
-    username = fields.Str()
-    password = fields.Str()
-    email = fields.Email()
-    account_type = fields.Str()
+    username = fields.Str(validate=[validate.Length(min=3, max=20)])
+    password = fields.Str(validate=[validate.Length(min=5, max=36)])
+    email = fields.Email(required=True)
+    account_type = fields.Str(required=True)
+
+
+class CarSchema(Schema):
+    id = fields.Int()
+    car_title = fields.Str(required=True)
+    title = fields.Str(required=True)
+    condition = fields.Str(required=True)
+    body_type = fields.Str(required=True)
+    brand = fields.Str(required=True)
+    model = fields.Str(required=True)
+    year_of_production = fields.Str(required=True)
+    country_origin = fields.Str(required=True)
+    country_now = fields.Str(required=True)
+    city = fields.Str(required=True)
+    mileage = fields.Str(required=True)
+    technical_condition = fields.Str(required=True)
+    gear_box = fields.Str(required=True)
+    fuel_type = fields.Str(required=True)
+    engine = fields.Str(required=True)
+    color = fields.Str(required=True)
+    safety = fields.Str(required=True)
+    wanted = fields.Str(required=True)
+    multimedia = fields.Str(required=True)
+    comfort = fields.Str(required=True)
+    picture = fields.Str(required=True)
+    date_added = fields.DateTime(dump_only=True)
+    user_id = fields.Nested(UserSchema)
 
 
 schema = UserSchema()
+car_schema = CarSchema()
 
 
 class UserData(Resource):
@@ -62,7 +112,9 @@ class UserData(Resource):
 
     @use_kwargs(user_args)
     def put(self, user_id, **kwargs):
-        user_updated = User.query.filter_by(id=user_id).update(kwargs) # 0 or 1 (if updated)
+        user_updated = User.query.filter_by(id=user_id).update(
+            kwargs
+        )  # 0 or 1 (if updated)
         if not user_updated:
             logger.error(f"\nNo User with id={user_id}")
             return {"Reason": "No such User", "User_id": user_id}, 404
@@ -89,5 +141,49 @@ class UserList(Resource):
         return result, 201
 
 
+class CarData(Resource):
+#    @use_kwargs(car_args)
+#    def get(self, color):
+#        import pdb
+#        pdb.set_trace()
+#        car = Car.query.filter_by(id=id)
+#        if car is None:  # Comparision id of objects
+#            return {"Reason": "Car not found", "Car_id": id}, 404
+#        result = car_schema.dump(car)
+#        logger.info(f"\nCar with id={result[id]} was called")
+#        return result.x
+#
+    def delete(self, id):
+        car = Car.query.get(id)
+        result = car_schema.dump(car)
+        try:
+            result["id"]
+        except KeyError:
+            logger.error(f"\nNo Car with id={id}")
+            return {"Reason": f"No Car with id={id}"}, 404
+        db.session.delete(car)
+        db.session.commit()
+        logger.info(f"\nCar with id={id} deleted")
+        return "", 204
+
+
+class CarList(Resource):
+    def get(self):
+        cars = Car.query.all()
+        list_of_cars = [car_schema.dump(car) for car in cars]
+        return {"Cars": list_of_cars}
+
+    @use_kwargs(car_args)
+    def post(self, **kwargs):
+        car = Car(**kwargs)
+        result = car_schema.dump(car)
+        db.session.add(car)
+        db.session.commit()
+        logger.info(f"\nCar for User with id={result['user_id']} added")
+        return result, 201
+
+
 api.add_resource(UserData, "/users/<int:user_id>")
 api.add_resource(UserList, "/users")
+api.add_resource(CarData, "/cars/<string:color>", "/cars/<int:id>")
+api.add_resource(CarList, "/cars")
