9349dfb441cbd43079790971d1e1ee5bd471701d
Roman D <blackworkarts@gmail.com>
2020-11-17 15:13:24 +0200

    Add ENV variables to PSQL

--- carapp/__init__.py
+++ carapp/__init__.py
@@ -2,13 +2,16 @@ import flask_bcrypt as fb
 from flask import Flask
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
+from os.path import expanduser
 from sqlalchemy import create_engine
 
 app = Flask(__name__)
 api = Api(app)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql+psycopg2://:I159'\
-        'postgres@localhost:5432/new_carapp_db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+with open(expanduser("~/.pgpass"), "r") as f:
+    host, port, database, user, password = f.read().split(":")
+DB_ADD = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}"
+app.config["SQLALCHEMY_DATABASE_URI"] = DB_ADD
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 bcrypt = fb.Bcrypt(app)
 
