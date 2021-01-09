5a9a9aa770406ad5e39c0f978e01b726c3411d4c
Roman D <blackworkarts@gmail.com>
2021-01-09 21:32:08 +0200

    Rearange endpoints(moved to from authent to endpoints file), changed login and logout endpoints

--- carapp/authent.py
+++ carapp/authent.py
@@ -1,8 +1,4 @@
-import flask
-import flask_login
-from . import app, auth, db_models, login_manager, token_serializer
-from flask import make_response, request
-from werkzeug.security import check_password_hash
+from carapp import auth, db_models, login_manager, token_serializer
 
 
 @login_manager.user_loader
@@ -14,69 +10,11 @@ def user_loader(user_id):
     return user
 
 
-# @login_manager.request_loader
-# def request_loader(request):
-#     username = request.authorization.username
-#     # import pdb; pdb.set_trace()
-#     user = db_models.User.query.filter_by(username=username).first()
-#     if not user:
-#         return None
-
-    # user.is_authenticated = check_password_hash(
-    #    user.password, request.authorization.password
-    # )
-#     return user
-
-
 @auth.verify_token
 def verify_token(token):
     try:
         data = token_serializer.loads(token)
-    except:  # noqa: E722
+    except:
         return False
-    if 'username' in data:
-        return data['username']
-
-
-@app.route("/")
-def index():
-    if "_user_id" in flask.session:
-        return "Logged in as " + flask_login.current_user.username
-
-    return "You are not logged in!"
-
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    check_auth = request.authorization
-    if not check_auth or not check_auth.username or not check_auth.password:
-        return make_response(
-            "Could not verify. Please Log In.",
-            401,
-            {"WWW-Authenticate": "Basic realm='Login required!'"},
-        )
-    user = db_models.User.query.filter_by(username=check_auth.username).first()
-    if not user:
-        return make_response(
-            "Could not verify user",
-            401,
-            {"WWW-Authenticate": "Basic realm='Login required!'"},
-        )
-    if check_password_hash(user.password, check_auth.password):
-        token = token_serializer.dumps({'username': user.username}).decode('utf-8')
-        flask_login.login_user(user)
-        return {"message": f"Logged in as a {user.username}.", "token": token}
-
-    return 401, {"message": "Bad login"}
-
-
-@app.route("/protected")
-@flask_login.login_required
-def protected():
-    return "Logged in as " + flask_login.current_user.username
-
-
-@app.route("/logout")
-def logout():
-    flask_login.logout_user()
-    return flask.redirect(flask.url_for("index"))
+    if "username" in data:
+        return data["username"]
--- carapp/endpoints.py
+++ carapp/endpoints.py
@@ -1,9 +1,10 @@
 import logging
-from flask_login import login_required
+import flask
+import flask_login
 from flask_restful import Resource
-from carapp import api, auth, db, db_models, schemes
+from carapp import api, app, auth, db, db_models, schemes, token_serializer
 from webargs.flaskparser import use_kwargs
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(message)s")
 logger = logging.getLogger(__file__)
@@ -21,6 +22,31 @@ class UserSignUp(Resource):
         result = schemes.USER_SCHEMA.dump(user)
         logger.info(f"\nUser with id={result['id']} created")
         return result, 201
+
+
+class UserLogin(Resource):
+    def post(self):
+        check_auth = flask.request.authorization
+        if not check_auth or not check_auth.username or not check_auth.password:
+            return {"message": "Could not verify. Please Log In."}, 401
+
+        user = db_models.User.query.filter_by(username=check_auth.username).first()
+        if not user:
+            return {"message": f"User {check_auth.username} doesn't exist."},
+        401
+        if check_password_hash(user.password, check_auth.password):
+            token = token_serializer.dumps({"username": user.username}).decode("utf-8")
+            flask_login.login_user(user)
+            return {"message": f"Logged in as a {user.username}.", "token": token}
+
+        return {"message": "Bad login"}, 401
+
+
+class UserLogout(Resource):
+    @auth.login_required
+    def post(self):
+        flask_login.logout_user()
+        return flask.redirect("/login", code=302)
 
 
 class UserData(Resource):
@@ -130,6 +156,8 @@ class CarList(Resource):
 
 
 api.add_resource(UserSignUp, "/signup", methods=["POST"])
+api.add_resource(UserLogin, "/login", methods=["POST"])
+api.add_resource(UserLogout, "/logout", methods=["POST"])
 api.add_resource(UserData, "/user", "/user/<int:user_id>")
 api.add_resource(UserList, "/users", methods=["GET"])
 api.add_resource(CarData, "/car", "/car/<int:id>")
